0141f1a3ad9e15ac33ee199b725cca30b4931aa1
phkuep <74210969+phkuep@users.noreply.github.com>
2020-11-18 21:42:29 +0100

    Update getSPON.py

--- scraper/src/getSPON.py
+++ scraper/src/getSPON.py
@@ -18,13 +18,14 @@ def article(article_url):
     return {'content': content, 'html': html}
 
 
-def article_comments(article_id: str):
-    return spon.comments.by_article_id(article_id)
+def article_comments(article_url):
+    art=article(article_url)
+    return spon.comments.by_article_id(art['content']['id'])
 
 
 if __name__ == '__main__':
-    print(article("https://www.spiegel.de/politik/ausland/barack-obama-soll-angela-merkel-zu-vierter-amtszeit-gedraengt-haben-a-1244446.html")["content"])
-    #print(article_comments("1244446")) Throws error !!!
+    print(article("https://www.spiegel.de/politik/deutschland/angela-merkel-und-ministerpraesidenten-zu-corona-jetzt-stehen-die-laender-unter-zugzwang-a-d0dcad88-ea71-4046-a914-7d7f5069e1c8")["content"])
+    print(article_comments("https://www.spiegel.de/politik/deutschland/angela-merkel-und-ministerpraesidenten-zu-corona-jetzt-stehen-die-laender-unter-zugzwang-a-d0dcad88-ea71-4046-a914-7d7f5069e1c8"))
 
 
 
