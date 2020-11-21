736810539f2d187d18607c5b8560725ac99960ca
hebecked <dustin.hebecker@gmail.com>
2020-11-21 11:22:16 +0100

    some mods not necessarily needed

---
+++ scraper/src/SpiegelOnlineScraper.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+from datetime import date
+import spiegel_scraper as spon
+
+class SponScraper(scraper):
+
+	def __init__():
+		pass
+
+	def archive(year: int, month: int, day: int):
+	    html = spon.archive.html_by_date(date(year, month, day))
+	    content = spon.archive.scrape_html(html)
+	    return {'content': content, 'html': html}
+	
+	
+	
+	
+	def article(article_url):
+	    html = spon.article.html_by_url(article_url)
+	    content = spon.article.scrape_html(html)
+	    return {'content': content, 'html': html}
+	
+	
+	def article_comments(article_id: str):
+	    return spon.comments.by_article_id(article_id)
+
+
+if __name__ == '__main__':
+    print(article("https://www.spiegel.de/politik/ausland/barack-obama-soll-angela-merkel-zu-vierter-amtszeit-gedraengt-haben-a-1244446.html")["content"])
+    #print(article_comments("1244446")) Throws error !!!
+
+
+
---
+++ scraper/src/TemplateScraper.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+from datetime import date
+
+
+class Scraper():
+
+	def __init__():
+		self.name = None
+		pass
+
+
+	def getNewArticles():
+		pass
+
+	def getArticle():
+		pass
+
+
+	def getCommentsForArticle():
+		pass
+
+
+	def _alreadyInDB():
+		pass
+
+
+
+	def __del__():
+		#call __del__ for subclass
+		pass
+
+
+
+
+if __name__ == '__main__':
+	pass
+
+
+
