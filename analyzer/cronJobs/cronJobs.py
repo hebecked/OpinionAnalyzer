479a4dc82535aa3b2d8676422d8959f37d04f5a9
mvogel1 <martin.m.vogel@daimler.com>
2020-11-21 12:42:34 +0100

    Adding latest version

--- analyzer/cronJobs/cronJobs.py
+++ analyzer/cronJobs/cronJobs.py
@@ -1,17 +1,22 @@
-from crontab import CronTab
+import pycron
+import time
 
 
-class cronJobs:
+class CronJobs:
 
+    def cronInit(self):
+        while True:
+            if pycron.is_now('* * * * *'):  # True Every minute
+                print('running nothing, because I am stupid (please kill me)')
+                time.sleep(60)  # The process should take at least 60 sec
+                # to avoid running twice or more in one minute
+            else:
+                time.sleep(15)
+
+
+"""
     def __init__(self):
-        """ initalize class and get connection parameters """
         self.jobs = [
             {"schedule": '* * * * *', "module": 'test.py'}  # Execute every minute "test.py"
         ]
-
-    def cronInit(self):
-        cron = CronTab(tabfile='/usr/src/app/cronJobs/crontab')
-        job = cron.new(command='echo hello_world')
-        job.minute.every(1)
-        cron.write()
-
+"""
