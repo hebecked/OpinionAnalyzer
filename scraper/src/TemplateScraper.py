942f0182c96bf45d6e18d6a88c37a8f120c1f976
phkuep <74210969+phkuep@users.noreply.github.com>
2020-11-21 20:05:07 +0100

    amending SPON to class structure
    working on the scraper template
    adding features to SpiegelOnlineScraper

--- scraper/src/TemplateScraper.py
+++ scraper/src/TemplateScraper.py
@@ -4,30 +4,52 @@ from datetime import date
 
 class Scraper():
 
-	def __init__():
-		self.name = None
-		pass
+    def __init__(self):
+        self.name = None#
+        #todo add database connection as class object?
+        #todo add local copy of database elements (urls) to compare with crawler results
+        #todo add id of source for use in database querys (e.g Spiegel, ZEIT)
+        pass
+    
+    def getNewArticles(self,start:date=date(1900,1,1), end:date=date.today()):
+        """
+        
+
+        Parameters
+        ----------
+        start : date, optional
+            DESCRIPTION. The default is date(1900,1,1).
+        end : date, optional
+            DESCRIPTION. The default is date.today().
+
+        Returns
+        -------
+        List of corresponding URLs from this source in string format
+
+        """      
+        pass
+
+    def getArticle(self, article_url):
+        pass
 
 
-	def getNewArticles():
-		pass
+    def getCommentsForArticle(self,article_url):
+        pass
 
-	def getArticle():
-		pass
-
-
-	def getCommentsForArticle():
-		pass
-
-
-	def _alreadyInDB():
-		pass
-
+#switch following functionality to database object?
+    def _alreadyInDB(self,article_url):
+        pass
+    
+    def _getArticlesFromDB(self):
+        pass
+    
+    def writeArticleToDB(self):
+        pass
 
 
-	def __del__():
+    def __del__(self):
 		#call __del__ for subclass
-		pass
+        pass
 
 
 
