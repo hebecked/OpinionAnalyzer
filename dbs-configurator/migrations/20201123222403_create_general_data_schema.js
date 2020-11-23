c95cc71059bcd187463271aec7150eb5f713e5c3
mvogel1 <martin.m.vogel@daimler.com>
2020-11-23 23:18:43 +0100

    Adding general_data schema and date_time_dimension to database

---
+++ dbs-configurator/migrations/20201123222403_create_general_data_schema.js
@@ -0,0 +1,14 @@
+const logger = require("tracer").console();
+
+exports.up = function(knex) {
+  return knex.schema.raw(
+      'CREATE SCHEMA IF NOT EXISTS general_data'
+  )
+      .then(function () {
+      logger.log("Successfully created general_data schema!");
+    });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable("test")
+};
