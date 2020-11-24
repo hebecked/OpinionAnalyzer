04b0c8a2094d7c7820736e4796bc137cd45e662d
hebecked <dustin.hebecker@gmail.com>
2020-11-24 23:26:28 +0100

    Imlemented a rudimentary test of NL-libs. Specifically a german offencive language classification from flair and multi language sentiment analysis from transformers. Also added JSON formated testdata to the analyzer.

--- scraper/dataCollectors/getSPON.py
+++ scraper/dataCollectors/getSPON.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 from datetime import date
 import spiegel_scraper as spon
+import json
 
 
 
@@ -26,6 +27,13 @@ def article_comments(article_url):
 if __name__ == '__main__':
     print(article("https://www.spiegel.de/politik/deutschland/angela-merkel-und-ministerpraesidenten-zu-corona-jetzt-stehen-die-laender-unter-zugzwang-a-d0dcad88-ea71-4046-a914-7d7f5069e1c8")["content"])
     print(article_comments("https://www.spiegel.de/politik/deutschland/angela-merkel-und-ministerpraesidenten-zu-corona-jetzt-stehen-die-laender-unter-zugzwang-a-d0dcad88-ea71-4046-a914-7d7f5069e1c8"))
+    articleData = article("https://www.spiegel.de/politik/deutschland/angela-merkel-und-ministerpraesidenten-zu-corona-jetzt-stehen-die-laender-unter-zugzwang-a-d0dcad88-ea71-4046-a914-7d7f5069e1c8")["content"]
+    commentData = article_comments("https://www.spiegel.de/politik/deutschland/angela-merkel-und-ministerpraesidenten-zu-corona-jetzt-stehen-die-laender-unter-zugzwang-a-d0dcad88-ea71-4046-a914-7d7f5069e1c8")
 
+    with open('TestArticle.json', 'w') as json_file:
+        json.dump(articleData, json_file)
+
+    with open('testComments.txt', 'w') as json_file:
+        json.dump(commentData, json_file)
 
 
