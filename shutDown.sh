abef819ea6b7dc37bd9a7ec5072fc163d284cd40
mvogel1 <martin.m.vogel@daimler.com>
2020-11-25 14:03:36 +0100

    Adding grafana volume removal

--- shutDown.sh
+++ shutDown.sh
@@ -3,5 +3,6 @@
 echo "Shutting down containers & cleaning up: "
 docker exec -u postgres postgres pg_ctl stop
 docker rm -f postgres dbs-configurator grafana analyzer scraper
+docker volume rm videowall_grafana_storage
 screen -X -S loggingSession quit
 screen -wipe
