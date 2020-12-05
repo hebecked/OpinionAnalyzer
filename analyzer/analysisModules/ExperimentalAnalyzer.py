219a9a503000c33c0d7823dc5cb4a08a1bdfbd66
hebecked <dustin.hebecker@gmail.com>
2020-12-05 17:39:10 +0100

    Merged both huggingface models. The third (textblob) model doesn't provide an error and was, therefore, not included. Further it is the least reliable one.

--- analyzer/analysisModules/ExperimentalAnalyzer.py
+++ analyzer/analysisModules/ExperimentalAnalyzer.py
@@ -7,15 +7,68 @@ TODO:
 - Compare different sentiment analyser and possibly combine to a ensemble approach to increase accuracy and obtain reasonable errors
 - Find additional analyser to run (5 personality traits, mood, IQ/education, wordcount vs sentiment, spelling, offensiveLanguage detection, ...)
 """
-from flair.models import TextClassifier
-from flair.data import Sentence
+#from flair.models import TextClassifier
+#from flair.data import Sentence
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from textblob_de import TextBlobDE as TextBlob
-import tensorflow as tf
+from scipy.special import softmax
+#import tensorflow as tf
 import numpy as np
 import json
 
-classifier = TextClassifier.load('de-offensive-language') # en-sentiment
+
+class multiling_bert_sentiment: 
+
+	def __init__(self):
+		#Based on Amazonreview (1-5 stars)		#https://huggingface.co/nlptown/bert-base-multilingual-uncased-sentiment?text=Nicht+kaufen 			
+		self.tokenizer = AutoTokenizer.from_pretrained("nlptown/bert-base-multilingual-uncased-sentiment")
+		self.model = AutoModelForSequenceClassification.from_pretrained("nlptown/bert-base-multilingual-uncased-sentiment")
+	
+	def analyze(self,text):
+		inputs = self.tokenizer(text, return_tensors="pt")
+		proOrCon = self.model(**inputs)
+		weights=proOrCon[0].detach().numpy()[0]
+		weights=softmax(weights)
+		average=np.average(np.linspace(1,5,5),weights=weights)
+		error=np.sqrt(np.average((np.linspace(1,5,5)-average)**2,weights=weights))*2./5.
+		average=average*2./5.-1
+		return [average,error]
+
+class german_bert_sentiment: 
+
+	def __init__(self):
+		#Based on a range of sources including twitter, facebook, product reviews #https://huggingface.co/oliverguhr/german-sentiment-bert?text=Du+Arsch%21
+		self.tokenizer = AutoTokenizer.from_pretrained("oliverguhr/german-sentiment-bert")
+		self.model = AutoModelForSequenceClassification.from_pretrained("oliverguhr/german-sentiment-bert")
+	
+	def analyze(self,text):
+		inputs = self.tokenizer(text, return_tensors="pt")
+		proOrCon = self.model(**inputs)
+		weights=proOrCon[0].detach().numpy()[0]
+		weights[2], weights[1] = weights[1], weights[2]
+		weights=softmax(weights)
+		average=np.average(np.linspace(1,-1,3),weights=weights)
+		error=np.sqrt(np.average((np.linspace(1,-1,3)-average)**2,weights=weights))
+		return [average,error]
+
+class Textblob_sentiment: 
+
+	#More information at https://textblob-de.readthedocs.io/en/latest/
+	def __init__(self):
+		pass
+		
+	def analyze(self,text):
+		blob = TextBlob(text)
+		mood = blob.sentiment
+		return mood.polarity
+
+	def abalyzeSubjectivity(self,text):
+		blob = TextBlob(text)
+		mood = blob.sentiment
+		return mood.subjectivity
+
+
+#classifier = TextClassifier.load('de-offensive-language') # en-sentiment
 
 with open('../Testdata/TestArticle.json') as f:
 	testArticle = json.load(f)
@@ -29,38 +82,38 @@ with open('../Testdata/TestComments.json') as f:
 #print(testComments)
 results=["positiv", "negativ", "neutral"]
 
-#Based on Amazonreview (1-5 stars)					
-tokenizer = AutoTokenizer.from_pretrained("nlptown/bert-base-multilingual-uncased-sentiment")
-model = AutoModelForSequenceClassification.from_pretrained("nlptown/bert-base-multilingual-uncased-sentiment")
-#Based on a range of sources including twitter, facebook, product reviews
-tokenizer2 = AutoTokenizer.from_pretrained("oliverguhr/german-sentiment-bert")
-model2 = AutoModelForSequenceClassification.from_pretrained("oliverguhr/german-sentiment-bert")
+#Based on Amazonreview (1-5 stars)	
+SentimentModel1=multiling_bert_sentiment()
 
-correlationMatrix=[[0]*3 for i in range(5)]
+#Based on a range of sources including twitter, facebook, product reviews
+SentimentModel2=german_bert_sentiment()
+
+testText="Du Arsch!" 
+
+
+result1=SentimentModel1.analyze(testText)
+result2=SentimentModel2.analyze(testText)
+result3=Textblob_sentiment().analyze(testText)
+results=np.array([result1, result2])
+finalResult=np.average(results.T[0], weights=1/results.T[1]**2)
+finalError=np.sqrt(1/np.mean(1/results.T[1]**2))
+print("Test: ", result1 , result2, result3, finalResult, finalError)
+
 
 i=0
 for comment in testComments:
 	if comment["user"] is not None and comment["body"] is not None:
-		sentence = Sentence(comment["body"])
-		Tourette = classifier.predict(sentence)
-		inputs = tokenizer(comment["body"], return_tensors="pt")
-		proOrCon = model(**inputs)
-		inputs2 = tokenizer2(comment["body"], return_tensors="pt")
-		proOrCon2 = model2(**inputs2)
-		blob = TextBlob(comment["body"])
-		mood = blob.sentiment
-		print( proOrCon, proOrCon2, mood )
-		#print("\n" + comment["user"]["username"] + ": " + comment["body"])
-		#print(str(i) + ": " +  \
-		#	" Text length: " + str(len(comment["body"])) + \
-		#	"; subcomments: " + str(len(comment["replies"])) + \
-		#	"; Swearing: " + str(Tourette) + \
-		#	"; Sentiment (star): " + str(1+proOrCon[0].argmax().tolist()) + "/" + results[proOrCon2[0][0].argmax()] )  #Mache ich heir das richtige?
-		stars=proOrCon[0].argmax().tolist()
-		rating=proOrCon2[0][0].argmax().tolist()
-		correlationMatrix[stars][rating]+=1
+		#sentence = Sentence(comment["body"])
+		#Tourette = classifier.predict(sentence)
+
+		
+
+		result1=SentimentModel1.analyze(comment["body"])
+		result2=SentimentModel2.analyze(comment["body"])
+		result3=Textblob_sentiment().analyze(comment["body"])
+		results=np.array([result1, result2])
+		finalResult=np.average(results.T[0], weights=1/results.T[1]**2)
+		finalError=np.sqrt(1/np.mean(1/results.T[1]**2))
+		print(result1 , result2, result3, finalResult, finalError)
+
 	i+=1
-	#if i == 10:
-	#	break
-for i in range(5):
-	print(correlationMatrix[i])
