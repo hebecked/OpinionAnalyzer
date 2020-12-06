6cc9ae86ed60d3369cdeba56f3af75f2c5c529f5
polarlyset <74265972+polarlyset@users.noreply.github.com>
2020-12-06 21:56:24 +0100

    insert for source_header (Spiegel) added
    insert for source_header (1 record: Spiegel) added

--- dbs-configurator/sqlStorage/static_data/insert_static_data.sql
+++ dbs-configurator/sqlStorage/static_data/insert_static_data.sql
@@ -1,7 +1,10 @@
 BEGIN;
 
+--do not change order of inserts
 INSERT INTO news_meta_data.source_type_header (type) VALUES('api');
 INSERT INTO news_meta_data.source_type_header (type) VALUES('crawl');
+--requires api to be inserted ahead of all other values into source_type_header as Spiegel needs source_id = 1
+INSERT INTO news_meta_data.source_header (type, source) VALUES(1,'Spiegel');
 INSERT INTO news_meta_data.object_header (type) VALUES('article_body');
 INSERT INTO news_meta_data.object_header (type) VALUES('comment');
 INSERT INTO news_meta_data.udf_header (udf_name) VALUES('headline');
