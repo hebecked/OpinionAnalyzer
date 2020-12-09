5c87c89db16fb07aae9ecfe0556c73337a11b5d0
polarlyset <74265972+polarlyset@users.noreply.github.com>
2020-12-09 21:17:39 +0100

    FK on udf_values amended, where clause on v_todo_crawl extended
    fk from object_header.id now on udf_values.object_type (was set on udf_id in error); condition "and proc_counter < 14" added to where clause of v_todo_crawl to filter out old articles that are not to be crawled again

--- dbs-configurator/sqlStorage/alter_tables/alter_tables.sql
+++ dbs-configurator/sqlStorage/alter_tables/alter_tables.sql
@@ -4,7 +4,7 @@ ALTER TABLE news_meta_data.source_header ADD FOREIGN KEY (type) REFERENCES news_
 ALTER TABLE news_meta_data.article_header ADD FOREIGN KEY (source_id) REFERENCES news_meta_data.source_header (id);
 ALTER TABLE news_meta_data.article_body ADD FOREIGN KEY (article_id) REFERENCES news_meta_data.article_header (id) ON DELETE CASCADE;
 ALTER TABLE news_meta_data.udf_values ADD FOREIGN KEY (udf_id) REFERENCES news_meta_data.udf_header (id);
-ALTER TABLE news_meta_data.udf_values ADD FOREIGN KEY (object_id) REFERENCES news_meta_data.object_header (id);
+ALTER TABLE news_meta_data.udf_values ADD FOREIGN KEY (object_type) REFERENCES news_meta_data.object_header (id);
 ALTER TABLE news_meta_data.comment ADD FOREIGN KEY (article_body_id) REFERENCES news_meta_data.article_body (id) ON DELETE CASCADE;
 ALTER TABLE news_meta_data.crawl_log ADD FOREIGN KEY (source_id) REFERENCES news_meta_data.source_header (id);
 
