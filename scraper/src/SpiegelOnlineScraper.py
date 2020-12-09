0f3cf2e131f7d0874dc985ea643815ad7e6798d0
phkuep <74210969+phkuep@users.noreply.github.com>
2020-12-09 21:33:05 +0100

    bug fixes
    adding try/except for spiegel-scraper requests
    reorganising fetch of comments (to get package of articles and comments close to each other)

--- scraper/src/SpiegelOnlineScraper.py
+++ scraper/src/SpiegelOnlineScraper.py
@@ -6,11 +6,15 @@ from comment import comment
 from databaseExchange import databaseExchange
 import TemplateScraper
 import time
+import math
 
 class SponScraper(TemplateScraper.Scraper):
-
+    SUBSET_LENGTH=10   #threshold for database flush
+    DELAY_SUBSET=1  #sleep x every SUBSET_LENGTH html requests
+    DELAY_INDIVIDUAL=0  #sleep x every html request
     def __init__(self):
         self.id=1 #set corresponding datasource id here
+        self.hasErrors=False
         pass
    
     def getArticlesList(self, start:date=date(1900,1,1), end:date=date.today()):
@@ -34,7 +38,11 @@ class SponScraper(TemplateScraper.Scraper):
         full_list=[]
         for dt in date_list:
             print("fetching date:",dt)
-            full_list+=(spon.archive.scrape_html(spon.archive.html_by_date(dt)))
+            try:
+                full_list+=(spon.archive.scrape_html(spon.archive.html_by_date(dt)))
+            except:
+                print("Article List crawl error!")
+                self.hasErrors=True
             time.sleep(1) #remove comment for crawler delay
         url_list=list(filter(lambda x: x['is_paid']==False,full_list)) #remove paid aricles without access
         for url in url_list:
@@ -44,53 +52,68 @@ class SponScraper(TemplateScraper.Scraper):
         return(returnList)	
 	        
     def getArticleDetails(self, art:article):
-        time.sleep(1)
-        html = spon.article.html_by_url(art.getArticle()['header']['url'])
-        content = spon.article.scrape_html(html)
-        if('id' in content.keys()):
+        try:
+            html = spon.article.html_by_url(art.getArticle()['header']['url'])
+            content = spon.article.scrape_html(html)
+        except:
+            print("Article crawl error!")
+            self.hasErrors=True
+            art.setObsolete(True)
+            return False
+        if('id' in content.keys()):            
             art.setBody({'headline':content['headline']['main'],'body':content['text'],'proc_timestamp':datetime.today(),'proc_counter':0})
+            art.freeData=content['id']
         #add udfs
-        for topic in content['topics']:
-            art.addUdf('label',topic)
-        for author in content['author']['names']:
-            art.addUdf('author',author)
-        art.addUdf('date_created',content['date_created'])
-        art.addUdf('date_modified',content['date_modified'])
-        art.addUdf('date_published',content['date_published'])
-        art.freeData=content['id']
+        if('topics' in content.keys()):
+            for topic in content['topics']:
+                art.addUdf('label',topic)
+        if('author' in content.keys()):
+            for author in content['author']['names']:
+                art.addUdf('author',author)
+        if('date_created' in content.keys()):
+            art.addUdf('date_created',content['date_created'])
+            try: art.setBodyCounter(max(int(math.log((date.today()-date.fromisoformat(content['date_created'][0:10])).days*24,2)),0))
+            except: print('Body Counter not set')
+        if 'date_modified' in content.keys(): art.addUdf('date_modified',content['date_modified'])
+        if 'date_published' in content.keys(): art.addUdf('date_published',content['date_published'])
         return True
     
     def getWriteArticlesDetails(self, writer:databaseExchange, articlesList:list):
         if(type(articlesList)!=list): return False
-        SUBSET_LENGTH=10
         start=0
         while start < len(articlesList):
-            for art in articlesList[start:(start+SUBSET_LENGTH)]:
-                print("fetching ",art.getArticle()['header']['url'])
+            for art in articlesList[start:(start+SponScraper.SUBSET_LENGTH)]:
+                print("fetching article:",art.getArticle()['header']['url'])
                 self.getArticleDetails(art)
-            writer.writeArticles(articlesList[start:(start+SUBSET_LENGTH)])
-            start+=SUBSET_LENGTH
+                time.sleep(SponScraper.DELAY_INDIVIDUAL)
+            writer.writeArticles(articlesList[start:(start+SponScraper.SUBSET_LENGTH)])
+            for art in articlesList[start:(start+SponScraper.SUBSET_LENGTH)]:
+                print("fetching comments for ",art.getArticle()['header']['url'])
+                cmts=self.getCommentsForArticle(art)
+                writer.writeComments(cmts)
+                time.sleep(SponScraper.DELAY_INDIVIDUAL)
+            start+=SponScraper.SUBSET_LENGTH
+            time.sleep(SponScraper.DELAY_SUBSET)
     
     def getCommmentExternalId(self,cmt:spon.comments):
-        print("hash:",hash(cmt['user']['username']+cmt['body']))
         return hash(cmt['user']['username']+cmt['body'])
 
-    def flattenComments(self, art:article,comments:list,parent:int,depth=0,start:date=date(1900,1,1),end:date=date.today()):
+    def flattenComments(self, art:article,comments:list,parent:int=None,depth=0,start:date=date(1900,1,1),end:date=date.today()):
         returnList=[]
         if type(comments)!=list: 
             return []
         for cmt in comments:
-            if (cmt['body']!=None and cmt['user']!=None):
-#                print(cmt)
+            if type(cmt)!=dict: continue
+            if (cmt['body']!=None and cmt['user']!=None and cmt['created_at']!=None):
                 cmt_id=self.getCommmentExternalId(cmt)
                 tmp=comment()
                 tmp.setData({"article_body_id":art.getArticle()["body"]["id"],"parent_id":parent,"level":depth,"body":cmt['body'],"proc_timestamp":datetime.today(),"external_id":cmt_id})
-                tmp.addUdf("replies",str(len(cmt['replies'])))
-                tmp.addUdf("author",cmt['user']['username'])
+                if "replies" in cmt.keys(): tmp.addUdf("replies",str(len(cmt['replies'])))
+                if "author" in cmt.keys():tmp.addUdf("author",cmt['user']['username'])
                 tmp.addUdf("created_at",cmt['created_at'])
                 if (start<=date.fromisoformat(cmt['created_at'][0:10])<=end):
                     returnList+=[tmp]
-                returnList+= self.flattenComments(art,cmt['replies'],cmt_id,depth+1,start,end)
+                if "replies" in cmt.keys(): returnList+= self.flattenComments(art,cmt['replies'],cmt_id,depth+1,start,end)
         return returnList
 
 	
@@ -100,49 +123,39 @@ class SponScraper(TemplateScraper.Scraper):
             articleId=art.freeData
         else:    
             articleId=self.getArticleDetails(art.getArticle()['header']['url']).freeData
-        comments=spon.comments.by_article_id(articleId)
-        returnList+=self.flattenComments(art,comments,None,0,start,end)
+        try:
+            comments=spon.comments.by_article_id(articleId)
+            returnList+=self.flattenComments(art,comments,None,0,start,end)
+        except:
+            print("Article crawl error!")
+            self.hasErrors=True
         return returnList
 
 
     def __del__(self):
         pass
-
-def test():
-    SpS=SponScraper()
-    test_article=SpS.getArticle("https://www.spiegel.de/politik/deutschland/angela-merkel-und-ministerpraesidenten-zu-corona-jetzt-stehen-die-laender-unter-zugzwang-a-d0dcad88-ea71-4046-a914-7d7f5069e1c8")["content"]
-    test_comments=SpS.getCommentsForArticle("https://www.spiegel.de/politik/deutschland/angela-merkel-und-ministerpraesidenten-zu-corona-jetzt-stehen-die-laender-unter-zugzwang-a-d0dcad88-ea71-4046-a914-7d7f5069e1c8",
-                                            date(2020,11,1),date.today())
-    start=date(2020,11,1)
-    end=date(2020,11,1)
-    for url in SpS.getArticlesList(start,end):
-       print(url,end="\n")
-    print("\narticle structure:")
-    print(test_article.keys())
-    print("\ncomment structure:")
-    print(test_comments[0].keys())
-    print("\nfirst level comments=",len(list(filter(lambda x:x['depth']==0,test_comments))),"\nall comments=",len(test_comments))
-   
-    SpS.__del__()    
+  
 
 if __name__ == '__main__':
+   starttime=datetime.today()
    SpS=SponScraper()
    db=databaseExchange()
    db.connect()
    db.logStartCrawl(SpS.id)
-   start=min(db.fetchLastRun(SpS.id).date(),date(2020,12,8))
+   start=max(db.fetchLastRun(SpS.id).date(),date(2020,12,1))
    end=date.today()
    articleHeaderList=SpS.getArticlesList(start,end)
    db.writeArticles(articleHeaderList)
    todo=db.fetchTodoList(SpS.id)
    SpS.getWriteArticlesDetails(db,todo)
-   for art in todo:
-       print("fetching comments for article:",art)
-       cmts=SpS.getCommentsForArticle(art)
-       db.writeComments(cmts)
-       time.sleep(1)
+#   for art in todo:
+#       print("fetching comments for article:",art)
+#       cmts=SpS.getCommentsForArticle(art)
+#       db.writeComments(cmts)
+#       time.sleep(1)
    
-   db.logEndCrawl()
+   db.logEndCrawl(not(SpS.hasErrors))
+   print("Laufzeit= ",datetime.today()-starttime)
    db.close()
 
 
