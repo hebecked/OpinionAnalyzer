43388531ac585cb8ea539d46b9779e990f0438eb
hebecked <dustin.hebecker@gmail.com>
2020-12-20 15:01:47 +0100

    Added some comments.

--- analyzer/analysisModules/AnnalyseComments.py
+++ analyzer/analysisModules/AnnalyseComments.py
@@ -3,10 +3,13 @@ from Sentiment import EnsembleSentiment
 from utils.databaseExchange import databaseExchange
 import json
 
-
+#init DB exchange module
 db=databaseExchange()
 
+#init the sentiment analyzer module
 sentiment_analyzer=EnsembleSentiment()
+
+#Fetch 1000 comments from the DB and anayze until all comments are analysed
 while(comments := db.fetchTodoListAnalyzer(1)):
 	sentiments=[]
 	for comment in comments:
