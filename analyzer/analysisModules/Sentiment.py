Fixed the overlenth token issue by splitting them every max_length and evaluating chunks individually. Individual results are merged by averaging them.

analyzer/analysisModules/Sentiment.py
@@ -31,23 +31,41 @@ class multilang_bert_sentiment:
 	#https://huggingface.co/nlptown/bert-base-multilingual-uncased-sentiment?text = Nicht+kaufen 
 	"""
 
-	def __init__(self):			
+	def __init__(self, truncate=False):			
 		self.tokenizer = AutoTokenizer.from_pretrained("nlptown/bert-base-multilingual-uncased-sentiment")
 		self.model = AutoModelForSequenceClassification.from_pretrained("nlptown/bert-base-multilingual-uncased-sentiment")
+		self.truncate=truncate
+		self.max_length=512
 	
 	def analyze(self, text):
-		try:
-			inputs = self.tokenizer(text, return_tensors = "pt") # , max_length=512
+		averages=[]
+		errors=[]
+		inputs = self.tokenizer(text, return_tensors = "pt")#, max_length=512, stride=0, return_overflowing_tokens=True, truncation=True, padding=True)
+		length=len(inputs['input_ids'][0])
+		while length>0:
+			if length>self.max_length:
+				next_inputs={k: (i[0][self.max_length:]).reshape(1,len(i[0][self.max_length:])) for k, i in inputs.items()}
+				inputs={k: (i[0][:self.max_length]).reshape(1,len(i[0][:self.max_length])) for k, i in inputs.items()}
+			else:
+				next_inputs=False
 			proOrCon = self.model(**inputs)
 			weights = proOrCon[0].detach().numpy()[0]
 			weights = softmax(weights)
 			average = np.average(np.linspace(1, 5, 5), weights = weights)
-			error = np.sqrt(np.average((np.linspace(1, 5, 5)-average)**2, weights = weights))*2./5.
 			average = average*2./5.-1
-		except:
-			average=0
-			error=1
-			print("Error caught in analyzer.")
+			averages.append(average)
+			errors.append(
+				np.sqrt(np.average((np.linspace(1, 5, 5)-average)**2, weights = weights))*2./5.
+				)
+			if self.truncate:
+				break
+			if next_inputs:
+				inputs=next_inputs
+			else:
+				break
+			length=len(inputs['input_ids'][0])
+		average = np.average(averages, weights = 1./np.array(errors)**2)
+		error = np.sqrt(1./np.sum(1./np.array(errors)**2))
 		return [average, error]
 
 
@@ -61,16 +79,20 @@ class german_bert_sentiment:
 		self.tokenizer = AutoTokenizer.from_pretrained("oliverguhr/german-sentiment-bert")
 		self.model = AutoModelForSequenceClassification.from_pretrained("oliverguhr/german-sentiment-bert")
 		self.truncate=truncate
+		self.max_length=512
 
 	def analyze(self, text):
-		#inputs = self.tokenizer(text, return_tensors = "pt")# , max_length=512
 		averages=[]
 		errors=[]
-		while True:
-			inputs = self.tokenizer(text, return_tensors = "pt", max_length=512, stride=0, return_overflowing_tokens=True, truncation=True, padding=True)
-			print(inputs)
-			text=inputs.pop('overflow_to_sample_mapping')
-			proOrCon = self.model(**inputs)
+		inputs = self.tokenizer(text, return_tensors = "pt")#, max_length=512, stride=0, return_overflowing_tokens=True, truncation=True, padding=True)
+		length=len(inputs['input_ids'][0])
+		while length>0:
+			if length>self.max_length:
+				next_inputs={k: (i[0][self.max_length:]).reshape(1,len(i[0][self.max_length:])) for k, i in inputs.items()}
+				inputs={k: (i[0][:self.max_length]).reshape(1,len(i[0][:self.max_length])) for k, i in inputs.items()}
+			else:
+				next_inputs=False
+			proOrCon = self.model(input_ids=inputs['input_ids'][0][:10].reshape(1,10), token_type_ids=inputs['token_type_ids'][0][:10].reshape(1,10), attention_mask=inputs['attention_mask'][0][:10].reshape(1,10))
 			weights = proOrCon[0].detach().numpy()[0]
 			weights[2], weights[1] = weights[1], weights[2]
 			weights = softmax(weights)
@@ -82,10 +104,11 @@ class german_bert_sentiment:
 			#from IPython import embed; embed()
 			if self.truncate:
 				break
-			if text.nelement() > 1:
-				print(text, "more", text.nelement(), flush=True)
+			if next_inputs:
+				inputs=next_inputs
 			else:
 				break
+			length=len(inputs['input_ids'][0])
 		average = np.average(averages, weights = 1./np.array(errors)**2)
 		error = np.sqrt(1./np.sum(1./np.array(errors)**2))
 		return [average, error]
@@ -105,7 +128,7 @@ class TextblobSentiment:
 		mood = blob.sentiment
 		return [mood.polarity, 1] #use an error of 1 for compatibility and because results are very unreliable
 
-	def abalyzeSubjectivity(self, text):
+	def analyzeSubjectivity(self, text):
 		blob = TextBlob(text)
 		mood = blob.sentiment
 		return mood.subjectivity
@@ -151,7 +174,7 @@ if __name__ ==  "__main__":
 	SentimentModel3 = EnsembleSentiment()
 
 
-	print("Running ", Test_cases, " tests.")
+	print("Running ", Test_cases, " tests + one overlength sample.")
 	accu=[]
 	for i, comment in enumerate(testComments):
 		if comment["user"] is not None and comment["body"] is not None:
@@ -166,7 +189,11 @@ if __name__ ==  "__main__":
 			print("Comment: ", i, " Results 1,2,3,4: ", result1, result2, result3, result4)
 		if i >= Test_cases:
 			break
-	print(len(accu))
-	print(SentimentModel3.analyze(accu))
+	accu=str().join(accu)
+	result1 = SentimentModel1.analyze(accu)
+	result2 = SentimentModel2.analyze(accu)
+	result3 = SentimentModel3.analyze(accu)
+	result4 = TextblobSentiment().analyze(accu)
+	print("Large Comment Results 1,2,3,4: ", result1, result2, result3, result4)
 	print("Tests completed successfully.")
 	