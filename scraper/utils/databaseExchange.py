f3d499ddb9816052776150a09aa1299d5201d872
phkuep <74210969+phkuep@users.noreply.github.com>
2021-01-03 22:12:16 +0100

    minor renaming

--- scraper/utils/databaseExchange.py
+++ scraper/utils/databaseExchange.py
@@ -290,7 +290,7 @@ class DatabaseExchange(connectDb.Database):
             if res[0] in topicizer_data.keys():
                 topicizer_data[res[0]]['udfs'].append(res[3])
             else:
-                topicizer_data[res[0]] = {'body' : res[1], 'headline' : res[2], 'udfs' : [res[3]]}
+                topicizer_data[res[0]] = {'body': res[1], 'headline': res[2], 'topics': [res[3]]}
         return topicizer_data
 
     def __fetch_analyzer_tables(self) -> dict:
