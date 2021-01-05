acc7ce05df7c30553239516e03e68e4013fc9ce8
mvogel1 <martin.m.vogel@daimler.com>
2021-01-05 04:51:33 +0100

    Making get_all_articles_from_dates dynamic according to the input dates

--- scraper/api/faz_api.py
+++ scraper/api/faz_api.py
@@ -1,15 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
+import datetime
+
 
 class FazApi:
 
-
-    def get_all_articles_from_date():
-        link_list_len = None
-        print(link_list_len)
+    def get_all_articles_from_dates(self, start, end):
+        start = datetime.datetime.strptime(start, '%Y-%m-%d').strftime('%d.%m.%Y')
+        end = datetime.datetime.strptime(end, '%Y-%m-%d').strftime('%d.%m.%Y')
+        print(start)
+        print(end)
         page = 1
         while True:
-            url = 'https://www.faz.net/suche/s' + str(page) + '.html?from=01.01.2021&to=01.01.2021'
+            url = 'https://www.faz.net/suche/s' + str(page) + '.html?from=' + str(start) + '&to=' + str(end)
             response = requests.get(url)
             soup = BeautifulSoup(response.content, 'lxml')
             objects = soup.find_all('li', {'class': 'lst-Teaser_Item'})
@@ -28,6 +31,6 @@ class FazApi:
 
 
 if __name__ == '__main__':
-    faz_api = FazApi
-    faz_api.get_all_articles_from_date()
+    faz_api = FazApi()
+    faz_api.get_all_articles_from_dates(start='2020-12-31', end='2021-01-01')
 
