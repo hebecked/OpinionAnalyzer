78dd791d47afedc4dc0fbe80d2061c165c8c243a
mvogel1 <martin.m.vogel@daimler.com>
2021-01-05 05:33:58 +0100

    Enforce type safety

--- scraper/api/faz_api.py
+++ scraper/api/faz_api.py
@@ -1,18 +1,40 @@
 import requests
 from bs4 import BeautifulSoup
 import datetime
+from dateutil.parser import parse
+
+
+def is_date(string: str, fuzzy=False) -> bool:
+    """
+    Return whether the string can be interpreted as a date.
+
+    :param string: str, string to check for date
+    :param fuzzy: bool, ignore unknown tokens in string if True
+    """
+    try:
+        parse(string, fuzzy=fuzzy)
+        return True
+
+    except ValueError:
+        return False
 
 
 class FazApi:
 
-    def get_all_articles_from_dates(self, start, end):
-        start = datetime.datetime.strptime(start, '%Y-%m-%d').strftime('%d.%m.%Y')
-        end = datetime.datetime.strptime(end, '%Y-%m-%d').strftime('%d.%m.%Y')
-        print(start)
-        print(end)
+    def get_all_articles_from_dates(self, start: str, end: str) -> []:
+        if not is_date(start):
+            print("Error: " + start + " has not the correct format. Please use format \'%YYYY-mm-dd\' as input format!")
+            raise Exception("DateError")
+        elif not is_date(end):
+            print("Error: " + end + " has not the correct format. Please use format \'%YYYY-mm-dd\' as input format!")
+            raise Exception("DateError")
+
+        link_array = []
+        start_date = datetime.datetime.strptime(start, '%Y-%m-%d').strftime('%d.%m.%Y')
+        end_date = datetime.datetime.strptime(end, '%Y-%m-%d').strftime('%d.%m.%Y')
         page = 1
         while True:
-            url = 'https://www.faz.net/suche/s' + str(page) + '.html?from=' + str(start) + '&to=' + str(end)
+            url = 'https://www.faz.net/suche/s' + str(page) + '.html?from=' + str(start_date) + '&to=' + str(end_date)
             response = requests.get(url)
             soup = BeautifulSoup(response.content, 'lxml')
             objects = soup.find_all('li', {'class': 'lst-Teaser_Item'})
@@ -22,15 +44,17 @@ class FazApi:
                 break
 
             for object in objects:
-                new_object = object.find('a', {'class': 'js-hlp-LinkSwap js-tsr-Base_ContentLink tsr-Base_ContentLink'}, href=True).attrs['href']
-                print("-------------------------------")
-                print(url)
-                print(new_object)
+                link = object.find('a', {'class': 'js-hlp-LinkSwap js-tsr-Base_ContentLink tsr-Base_ContentLink'}, href=True).attrs['href']
+                if link not in link_array:
+                    link_array.append(link)
 
             page += 1
+
+        print(link_array)
+        return link_array
 
 
 if __name__ == '__main__':
     faz_api = FazApi()
-    faz_api.get_all_articles_from_dates(start='2020-12-31', end='2021-01-01')
+    list_of_links = faz_api.get_all_articles_from_dates(start='202012-31', end='2021-01-01')
 
