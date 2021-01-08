27f6b17fccd948a27a35139a2456cf166e7b1b9f
mvogel1 <martin.m.vogel@daimler.com>
2021-01-08 18:46:16 +0100

    Adding extended faz_api functionality (beta)

--- scraper/api/faz_api.py
+++ scraper/api/faz_api.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import datetime
 from dateutil.parser import parse
+import json
 
 
 def is_date(string: str, fuzzy=False) -> bool:
@@ -23,9 +24,9 @@ class FazApi:
 
     def get_all_articles_from_dates(self, start: str, end: str) -> []:
         if not is_date(start):
-            raise Exception("Error: " + start + " incorrect format. Please use format \'%YYYY-mm-dd\' as input format!")
+            raise Exception(start + " (start) has incorrect format. Please use format \'%YYYY-mm-dd\' as input format!")
         elif not is_date(end):
-            raise Exception("Error: " + end + " incorrect format. Please use format \'%YYYY-mm-dd\' as input format!")
+            raise Exception(end + "(end) has incorrect format. Please use format \'%YYYY-mm-dd\' as input format!")
 
         link_array = []
         start_date = datetime.datetime.strptime(start, '%Y-%m-%d').strftime('%d.%m.%Y')
@@ -50,23 +51,42 @@ class FazApi:
 
         return link_array
 
-    def get_article_meta(self, url) -> {}:
-        print(url)
-        response = requests.get(url)
+    def get_article_meta(self, url: str) -> {}:
+        article_url = url + "?printPagedArticle=true#pageIndex_1"
+        response = requests.get(article_url)
         soup = BeautifulSoup(response.content, 'lxml')
-        print(soup)
 
-        #<p class="First atc-TextParagraph" id="pageIndex_1"
-        #<p class="atc-TextParagraph"
-        #<h3 class="atc-SubHeadline
-        #
+        article_meta = soup.find('div', {'class': 'js-adobe-digital-data is-Invisible'}).attrs['data-digital-data']
+        article_meta_json = json.loads(article_meta)
+        article_meta_json_formatted = json.dumps(article_meta_json, indent=2)
+
+        return article_meta_json_formatted
+
+    def get_article_body_meta(self, url: str) -> {}:
+        article_url = url + "?printPagedArticle=true#pageIndex_1"
+        response = requests.get(article_url)
+        soup = BeautifulSoup(response.content, 'lxml')
+        article_body = soup.find_all('script', {'type': 'application/ld+json'})
+        article_body_meta = article_body[1].contents[0].rstrip("\n").rstrip("\t").strip()
+        article_body_meta_json = json.loads(article_body_meta)
+        article_body_meta_json_formatted = json.dumps(article_body_meta_json, indent=2)
+
+        return article_body_meta_json_formatted
+
+    def get_article_comments(self, url: str) -> []:
 
         print('---------- Comments -----------')
         comment_response = requests.get('https://www.faz.net/aktuell/feuilleton/buecher/rezensionen/sachbuch/biographie-des-schriftstellers-leonhard-frank-17092115.html?action=commentList&page=1&onlyTopArguments=false&ot=de.faz.ArticleCommentsElement.comments.ajax.ot')
         comment_soup = BeautifulSoup(comment_response.content, 'lxml')
         print(comment_soup)
+        comment_title_soup = comment_soup.find_all('p', {'class': 'js-lst-Comments_CommentTitle lst-Comments_CommentTitle'})
+        comment_body_soup = comment_soup.find_all('p', {'class': 'lst-Comments_CommentText'})
 
-        return {}
+        print(comment_title_soup)
+        print(comment_body_soup)
+
+
+        return []
 
 
 
@@ -74,6 +94,8 @@ class FazApi:
 if __name__ == '__main__':
     faz_api = FazApi()
     list_of_links = faz_api.get_all_articles_from_dates(start='2021-01-01', end='2021-01-01')
-    faz_api.get_article_meta(url=list_of_links[0])
+    article_meta = faz_api.get_article_meta(url=list_of_links[0])
+    article_body_meta = faz_api.get_article_body_meta(url=list_of_links[0])
+    article_comments = faz_api.get_article_comments(url=list_of_links[0])
 
 
