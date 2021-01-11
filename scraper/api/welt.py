a069a056abfdaae955d5f49ea4ef7b8deac62f08
mvogel1 <martin.m.vogel@daimler.com>
2021-01-11 14:43:54 +0100

    Fully functional welt api

--- scraper/api/welt.py
+++ scraper/api/welt.py
@@ -2,8 +2,6 @@ import datetime
 import json
 from bs4 import BeautifulSoup
 from dateutil.parser import parse
-import time
-from selenium import webdriver
 import urllib.request
 
 
@@ -21,28 +19,49 @@ def is_date(string: str, fuzzy=False) -> bool:
         return False
 
 
+def get_article_comments_recursively(url: str, comments: [], minimum_created: datetime.datetime) -> []:
+    min_created_time = minimum_created.strftime("%Y-%m-%dT%H:%M:%S.%f")
+    comment_url = url + '&created-cursor=' + str(min_created_time)
+
+    with urllib.request.urlopen(comment_url) as response:
+        comments_rec = response.read().decode('utf-8')
+
+    comments_rec_json = json.loads(comments_rec)
+
+    if len(comments_rec_json['comments']) == 0:
+        return comments
+
+    elif len(comments_rec_json['comments']) == 1:
+        comments.append(comments_rec_json['comments'])
+        return comments
+
+    else:
+        for comment in comments_rec_json['comments']:
+            comment_created = datetime.datetime.strptime(comment['created'], "%Y-%m-%dT%H:%M:%S.%f")
+            if comment_created < minimum_created:
+                minimum_created = comment_created
+            comments.append(comment)
+        return get_article_comments_recursively(url=url, comments=comments, minimum_created=minimum_created)
+
 class Welt:
 
-    def get_all_articles_from_dates(self, start: str, end: str) -> []:
+    def __init__(self):
+        self.article_url = 'https://www.welt.de/schlagzeilen/nachrichten-vom-'
+        self.comment_url = 'https://api-co.la.welt.de/api/comments?document-id='
+
+    # Use datetime.date as type for start and end str
+    def get_all_articles_from_dates(self, start: datetime.datetime, end: datetime.datetime) -> []:
         """
         Return a list of all article links created in the time interval set
-        :param start: str, start date of the intervall in YYYY-mm-dd format
-        :param end: str, end date of the intervall in YYYY-mm-dd format
+        :param start: str, start date of the interval in YYYY-mm-dd format
+        :param end: str, end date of the interval in YYYY-mm-dd format
         """
-        # Checking if date strings have correct format
-        if not is_date(start):
-            raise Exception(start + " (start) has incorrect format. Please use format \'%YYYY-mm-dd\' as input format!")
-        elif not is_date(end):
-            raise Exception(end + "(end) has incorrect format. Please use format \'%YYYY-mm-dd\' as input format!")
-
         # Creating time interval to fetch articles from
-        start_interval = datetime.datetime.strptime(start, '%Y-%m-%d')
-        end_interval = datetime.datetime.strptime(end, '%Y-%m-%d')
         step = datetime.timedelta(days=1)
         dates_to_search = []
-        while start_interval <= end_interval:
-            dates_to_search.append(str(start_interval.date()))
-            start_interval += step
+        while start <= end:
+            dates_to_search.append(str(start.date()))
+            start += step
 
         link_array = []
 
@@ -51,11 +70,9 @@ class Welt:
             # Transforming publish_date variable into format to request data from welt.de
             start_date = datetime.datetime.strptime(publish_date, '%Y-%m-%d').strftime('%d-%m-%Y')
 
-            # https://www.welt.de/schlagzeilen/nachrichten-vom-1-1-2021.html
-
             # Retrieving all article links from time interval via the search HTML
 
-            url = 'https://www.welt.de/schlagzeilen/nachrichten-vom-' + str(start_date) + '.html'
+            url = self.article_url + str(start_date) + '.html'
             with urllib.request.urlopen(url) as response:
                 html = response.read().decode('utf-8')
             soup = BeautifulSoup(html, 'lxml')
@@ -74,45 +91,57 @@ class Welt:
         Return a JSON with meta data related to a single article
         :param url: str, url of a article
         """
-
-        """
-        # Get full content of a article on one page
-        article_url = url + "?printPagedArticle=true#pageIndex_1"
-
-        # Retrieve full article HTML and transform into soup
-        response = requests.get(article_url)
-        soup = BeautifulSoup(response.content, 'lxml')
-
-        # Select only article meta data of article
-        article_meta_data = soup.find('div', {'class': 'js-adobe-digital-data is-Invisible'}).attrs['data-digital-data']
-        article_meta_data_json = json.loads(article_meta_data)
-        article_meta_data_json_formatted = json.dumps(article_meta_data_json, indent=2)
-        """
-        print(url)
+        # Retrieving full html from Url and transforming into soup
         with urllib.request.urlopen(url) as response:
             html = response.read().decode('utf-8')
         soup = BeautifulSoup(html, 'lxml')
-        #print(soup)
+
+        # Retrieving meta data
         article_meta_data = soup.find('script', {'data-qa': 'StructuredData', 'type': 'application/ld+json'}).contents[0]
         article_meta_data_json = json.loads(article_meta_data)
         article_meta_data_json_formatted = json.dumps(article_meta_data_json, indent=2)
-        print('----------------------------------- JSON -------------------------')
-        print(article_meta_data_json_formatted)
 
         return article_meta_data_json_formatted
 
     def get_article_comments(self, url: str) -> []:
         """
-        Return a Array with comments meta data of a single article
+        Return a Array with comments meta data of a single article,
+        because of API limitations it can only return maximum 100 comments per article
         :param url: str, url of a article
         """
+        # https://api-co.la.welt.de/api/comments?document-id=223620336&limit=100
 
-        return []
+        try:
+            article_id = url.split('article')[1].split('/')[0]
+        except IndexError:
+            article_id = url.split('plus')[1].split('/')[0]
+
+        comment_url = self.comment_url + article_id + '&limit=100&sort=NEWEST'
+        comments_list = []
+
+        with urllib.request.urlopen(comment_url) as response:
+            comments = response.read().decode('utf-8')
+
+        comment_json = json.loads(comments)
+
+        min_comment_created = datetime.datetime.now()
+        for comment in comment_json['comments']:
+            comment_created = datetime.datetime.strptime(comment['created'], "%Y-%m-%dT%H:%M:%S.%f")
+            if comment_created < min_comment_created:
+                min_comment_created = comment_created
+            comments_list.append(comment)
+
+        comments_list = get_article_comments_recursively(url=comment_url, comments=comments_list, minimum_created=min_comment_created)
+
+        return comments_list
 
 
 if __name__ == '__main__':
     welt_api = Welt()
-    link_list = welt_api.get_all_articles_from_dates(start='2021-01-01', end='2021-01-01')
-    #welt_api.get_article_meta(url='https://www.welt.de/vermischtes/article223586640/Leipzig-Bundeswehr-Jeeps-in-Leipzig-angezuendet-und-ausgebrannt.html')
-    for link in link_list:
-        welt_api.get_article_meta(link)
+    link_list = welt_api.get_all_articles_from_dates(start=datetime.datetime(2021,1,1), end=datetime.datetime(2021,1,1))
+    print(link_list)
+    for article_link in link_list:
+        a_json = welt_api.get_article_meta(article_link)
+        print(a_json)
+    all_comments = welt_api.get_article_comments(link_list[0])
+    print(all_comments)
