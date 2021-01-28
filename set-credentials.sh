9d045b2b2eb9c3d7b548f6929af45dec2f08d696
mvogel1 <martin.m.vogel@daimler.com>
2021-01-28 16:09:07 +0100

    Adding first part of system logic: - Enabling viewer rights with limited permissions for anonymous users - Configuring docker-compose to use env variables to parse confidential (server local) credentials to containers

---
+++ set-credentials.sh
@@ -0,0 +1,15 @@
+# Run using "source set-credentials.sh"
+# Please set this accordingly before using "./run.sh"
+
+echo "Setting credentials for project"
+
+echo "Setting Postgres Admin Credentials"
+export POSTGRES_USER="postgres"
+export POSTGRES_PASSWORD="postgres"
+
+echo "Setting Grafana Postgres User (only SELECT rights to specific schemas inside postgres)"
+export GF_PG_USER="gf_postgres"
+export GF_PG_PASSWORD="gf_postgres"
+
+echo "Grafana Admin Configs"
+export GF_SECURITY_ADMIN_PASSWORD="IamTheAdmin"
