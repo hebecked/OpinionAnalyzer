de7caff9614de3f8469f368b29e279bc3eb5f515
mvogel1 <martin.m.vogel@daimler.com>
2021-02-04 16:47:37 +0100

    Delete redundant alertmanger from ./shut-down.sh

--- shut-down.sh
+++ shut-down.sh
@@ -2,7 +2,7 @@
 
 echo "Shutting down containers & cleaning up: "
 docker exec -u postgres postgres pg_ctl stop
-docker rm -f postgres dbs-configurator grafana analyzer scraper pgadmin node-exporter prometheus cadvisor alertmanager
+docker rm -f postgres dbs-configurator grafana analyzer scraper pgadmin node-exporter prometheus cadvisor
 docker volume rm opinionanalyzer_grafana_storage
 screen -X -S loggingSession quit
 screen -wipe
