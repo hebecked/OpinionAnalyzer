2a24c346189e87c28ced6b53718b793bf69f0782
hebecked <dustin.hebecker@gmail.com>
2021-02-06 23:45:56 +0100

    Debugging for remote machine.

--- analyzer/analysisModules/Train_own_sentiment_NN.py
+++ analyzer/analysisModules/Train_own_sentiment_NN.py
@@ -185,12 +185,12 @@ trainer.train()
 #torch.save(model.state_dict(), 'results/bert_self_trained_model')
 model.save_pretrained("./results/bert_self_trained_model")
 os.remove("million_post_corpus/corpus.sqlite3") 
-
+os.removedirs("million_post_corpus") 
 
 print("Testing:")
 model.eval()
 for test_data in test_dataset: 
-    print(model(test_data))
+    print(model(**test_data))
 
 
 #The following contains different versions of the SQL Query
