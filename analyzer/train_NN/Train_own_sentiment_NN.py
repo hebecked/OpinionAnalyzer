2f39c25d5b31cb8f4ef09dfd2155defe79330ecb
hebecked <dustin.hebecker@gmail.com>
2021-02-13 20:03:38 +0100

    Inter machine debugging

--- analyzer/train_NN/Train_own_sentiment_NN.py
+++ analyzer/train_NN/Train_own_sentiment_NN.py
@@ -24,6 +24,7 @@ class BertForMultiLabelSequenceClassification(BertForSequenceClassification): # 
         self.num_labels = num_labels
         self.dropout = torch.nn.Dropout(self.config.hidden_dropout_prob)
         self.classifier = torch.nn.Linear(self.config.hidden_size, num_labels)
+        self.init_weights()
         #self.apply(self.init_bert_weights)
         config = self.config.to_dict()
         config["_num_labels"] = 3
@@ -36,17 +37,52 @@ class BertForMultiLabelSequenceClassification(BertForSequenceClassification): # 
         config["model_type"]= "bert"
         self.config = PretrainedConfig.from_dict(config)
  
-    def forward(self, input_ids, token_type_ids=None, attention_mask=None, labels=None):
-        pooled_output = self.bert(input_ids, token_type_ids, attention_mask) # , output_all_encoded_layers=False # outdated actually to be placed in init or config under the name output_hidden_states
-        pooled_output = self.dropout(pooled_output[1])
+    def forward(self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+    ):
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        outputs = self.bert(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+
+        pooled_output = outputs[1]
+
+        pooled_output = self.dropout(pooled_output)
         logits = self.classifier(pooled_output)
 
+        loss = None
         if labels is not None:
             loss_fct = BCEWithLogitsLoss()
-            loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1, self.num_labels))
-            return loss
-        else:
-            return logits
+            loss = loss_fct(logits.view(-1), labels.view(-1)) #, self.num_labels
+
+        if not return_dict:
+            output = (logits,) + outputs[2:]
+            return ((loss,) + output) if loss is not None else output
+
+        return SequenceClassifierOutput(
+            loss=loss,
+            logits=logits,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
         
     def freeze_bert_encoder(self):
         for param in self.bert.parameters():
