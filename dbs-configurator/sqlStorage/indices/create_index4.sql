ac0834b9f5907239059348299292714f2c8e4859
polarlyset <74265972+polarlyset@users.noreply.github.com>
2021-02-13 22:46:56 +0100

    index added on 2 tbls for text search
    GIN indices added to comment and article_body for speedup of text search

---
+++ dbs-configurator/sqlStorage/indices/create_index4.sql
@@ -0,0 +1,8 @@
+BEGIN;
+
+CREATE INDEX uv_udf_id ON news_meta_data.udf_value(udf_id);
+CREATE INDEX co_body ON news_meta_data.comment USING GIN (to_tsvector('german',body));
+CREATE INDEX ab_body ON news_meta_data.article_body USING GIN (to_tsvector('german',body));
+
+
+COMMIT;
