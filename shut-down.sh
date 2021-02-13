a0c6b982c51b671e044cfd015e00fac47d2871d8
phkuep <74210969+phkuep@users.noreply.github.com>
2021-02-13 22:36:47 +0100

    shutdown new container

--- shut-down.sh
+++ shut-down.sh
@@ -2,7 +2,7 @@
 
 echo "Shutting down containers & cleaning up: "
 docker exec -u postgres postgres pg_ctl stop
-docker rm -f postgres dbs-configurator grafana analyzer scraper pgadmin node-exporter prometheus cadvisor
+docker rm -f postgres dbs-configurator grafana analyzer scraper pgadmin node-exporter prometheus cadvisor topic_detector
 docker volume rm opinionanalyzer_grafana_storage
 screen -X -S loggingSession quit
 screen -wipe
