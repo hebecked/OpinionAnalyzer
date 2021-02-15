4088885bf1dd2efd9e37f3ba98213c0b6833ef58
polarlyset <74265972+polarlyset@users.noreply.github.com>
2021-02-15 23:27:06 +0100

    view added
    news_meta_data.v_todo_lemmatizer added
    (displays latest 1000 article bodies that are next in queue to be analyzed)

---
+++ dbs-configurator/sqlStorage/views/create_views20210215.sql
@@ -0,0 +1,16 @@
+BEGIN;
+
+create or replace view news_meta_data.v_todo_lemmatizer as (select
+	ah.source_date,
+	ab.id as ab_id,
+	ab.headline,
+	ab.body
+from news_meta_data.article_header ah,
+	news_meta_data.article_body ab
+left join news_meta_data.lemma_count lc on ab.id = lc.article_body_id
+where ah.id=ab.article_id
+and lc.lemma_id is null
+order by ah.source_date desc
+limit 1000);
+
+COMMIT;
