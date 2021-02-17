4961a3dd157a410a5192ebb61ffdfc80b5a27c81
phkuep <74210969+phkuep@users.noreply.github.com>
2021-02-17 20:40:30 +0100

    amended view to filter empty bodies
    also changed run length and rerun cron jobs

--- dbs-configurator/sqlStorage/views/create_views20210215.sql
+++ dbs-configurator/sqlStorage/views/create_views20210215.sql
@@ -10,6 +10,7 @@ from news_meta_data.article_header ah,
 left join news_meta_data.lemma_count lc on ab.id = lc.article_body_id
 where ah.id=ab.article_id
 and lc.lemma_id is null
+and ab.body != ''
 order by ah.source_date desc
 limit 1000);
 
--- topic_detector/analysisModules/AnalyseArticles.py
+++ topic_detector/analysisModules/AnalyseArticles.py
@@ -13,22 +13,24 @@ db = DatabaseExchange()
 #init baseline topic detection for analysis
 bltd = BaselineTopicDetection(False)
 
-#add a loop here
-articles = db.fetch_topicizer_data()  # list of {article_body_id : dict {body: , headline: ,topics: } }
-result_list = []
-for article_body_id, article in articles.items():
-	analyze = article["headline"] + " " + article["body"]
-	
-	#determine topics
-	result = bltd.get_word_frequency(analyze, True)
-	print(result)  # accumulate and write as a block to DB
-	for lemma, count in result.items():
-		if 2 <= len(lemma) <= 30:
-			result_list += [{'lemma': lemma, 'article_body_id': article_body_id, 'lemma_count': count}]
-	# write type: dict(lemma, article_body_id, lemma_count)
-if db.write_lemmas(result_list):
-	print("successfully written to db")
-else:
-	print("some error writing")
+for counter in range(50):
+	result_list = []
+	articles = db.fetch_topicizer_data()  # list of {article_body_id : dict {body: , headline:} }
+	if len(articles) == 0:
+		break
+	for article_body_id, article in articles.items():
+		analyze = article["headline"] + " " + article["body"]
+
+		#determine topics
+		result = bltd.get_word_frequency(analyze, True)
+		print(result)  # accumulate and write as a block to DB
+		for lemma, count in result.items():
+			if 2 <= len(lemma) <= 30:
+				result_list += [{'lemma': lemma, 'article_body_id': article_body_id, 'lemma_count': count}]
+		# write type: dict(lemma, article_body_id, lemma_count)
+	if db.write_lemmas(result_list):
+		print("successfully written to db")
+	else:
+		print("some error writing")
 db.close()
 
