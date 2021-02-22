de7808658db4700904d93a304fc32c6a0ba69d31
polarlyset <74265972+polarlyset@users.noreply.github.com>
2021-02-22 21:09:40 +0100

    minor amendment
    previous gin indices removed without incremental release (long building time, low odds that anybody has already implemented them)

--- dbs-configurator/sqlStorage/indices/create_index5.sql
+++ dbs-configurator/sqlStorage/indices/create_index5.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-CREATE EXTENSION pg_trgm;
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
 CREATE INDEX IF NOT EXISTS co_body_trgm ON news_meta_data.comment USING gin (body gin_trgm_ops);
 CREATE INDEX IF NOT EXISTS ab_body_trgm ON news_meta_data.article_body USING gin (body gin_trgm_ops);
 CREATE INDEX IF NOT EXISTS co_dt_crtd ON news_meta_data.comment(date_created);
--- dbs-configurator/sqlStorage/indices/drop_index.sql
+++
@@ -1,6 +0,0 @@
-BEGIN;
-
-DROP INDEX IF EXISTS news_meta_data.co_body;
-DROP INDEX IF EXISTS news_meta_data.ab_body;
-
-COMMIT;
