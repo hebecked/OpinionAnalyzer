ab909b6abe4b033e284df7df1d3d6a705f7d961b
polarlyset <74265972+polarlyset@users.noreply.github.com>
2021-02-24 11:07:08 +0100

    name conflict at rebase resolved (alter_tables4 -> 6)
    no functional changes

--- dbs-configurator/migrations/20210222181900_alter_tables6.js
+++ dbs-configurator/migrations/20210222181900_alter_tables6.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 const logger = require("tracer").console();
 
-const query = fs.readFileSync("/usr/src/app/sqlStorage/alter_tables/alter_tables4.sql");
+const query = fs.readFileSync("/usr/src/app/sqlStorage/alter_tables/alter_tables6.sql");
 
 exports.up = function(knex) {
   return knex.schema.raw(query.toString())
       .then(function () {
-      logger.log("Successfully added constraints!");
+      logger.log("Successfully added columns!");
     });
 };
 
