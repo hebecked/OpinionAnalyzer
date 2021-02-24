8ab870be034d62b1d6d69846418f655067fa56da
polarlyset <74265972+polarlyset@users.noreply.github.com>
2021-02-24 10:56:03 +0100

    index added, condition added to view
    comment.proc_timestamp index added
    v_comment_date_created_empty adjusted: condition added to only select comment records with proc_timestamp older than 3 hours to ensure udf_values have been populated and their values used for updating, before fall back source_date in article_header gets selected

--- dbs-configurator/sqlStorage/views/create_views2.sql
+++ dbs-configurator/sqlStorage/views/create_views2.sql
@@ -1,14 +1,17 @@
 BEGIN;
 
---topic detection with sources 
-create or replace view news_meta_data.v_comment_date_created_empty as (
-select
-ah.id as ah_id,
-ab.id as ab_id,
-co.id as co_id
-from news_meta_data.comment co, news_meta_data.article_body ab, news_meta_data.article_header ah
-where ah.id=ab.article_id
-and ab.id=co.article_body_id
-and co.date_created is null);
+--show all comment records without date_created that are older than 3 hrs processing time
+CREATE OR REPLACE VIEW news_meta_data.v_comment_date_created_empty as (
+SELECT ah.id AS ah_id,
+    ab.id AS ab_id,
+    co.id AS co_id
+   FROM news_meta_data.comment co,
+    news_meta_data.article_body ab,
+    news_meta_data.article_header ah
+  WHERE ah.id = ab.article_id 
+  AND ab.id = co.article_body_id 
+  AND co.date_created IS NULL
+  AND co.proc_timestamp < (NOW() - INTERVAL '3 hours')
+);
 
 COMMIT;
